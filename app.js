e127af732b28676484e7586f9a26258a96d49208
meirwitt <meirwitt@gmail.com>
2017-11-22 21:58:42 +0200

    first initializations

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(require('./controllers'))
 
+
+
+
 app.listen(port, function() {
   console.log('Listening on port ' + port)
 })
