326f60ae73428665160a8afd966faf352271de06
Artium <artium@nihamkin.com>
2017-11-22 12:09:33 +0200

    Initial commit. Skeleton project of node + express based on a previous mission-plan project.

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+  , router = express.Router()
+
+router.use('/missions', require('./missions'))
+
+router.get('/', function(req, res) {
+  res.render('index')
+})
+
+module.exports = router
---
+++ controllers/missions.js
@@ -0,0 +1,11 @@
+﻿var express = require('express')
+    , router = express.Router()
+    , Mission = require('../models/mission')
+
+router.get('/:id', function (req, res) {
+    Mission.get(req.params.id, function (err, comment) {
+        res.render('missions/mission', { id: req.params.id, comment: comment })
+    })
+})
+
+module.exports = router
