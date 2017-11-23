423b4d87d40533702e39502a890a79e0cc3df3cc
adirpr <adir.prager@gmail.com>
2017-11-24 01:18:11 +0200

    Adir & Barak: Added function which returns question meta data

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
     tq = require('./lib/TriviaQuestions.js'),
     players = require('./lib/Players.js'),
     PORT = process.env.PORT || 8080,
-    url  = 'http://localhost:' + PORT + '/';
+    url = 'http://localhost:' + PORT + '/';
 
 if (process.env.SUBDOMAIN) {
     url = 'http://' + process.env.SUBDOMAIN + '.jit.su/';
@@ -23,11 +23,11 @@ players.init();
 
 app.use('/css/', express.static(__dirname + '/css'));
 app.use('/js/', express.static(__dirname + '/js'));
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/p', function (req, res) {
+app.get('/p', function(req, res) {
     res.sendfile(__dirname + '/player.html');
 });
 
@@ -38,34 +38,34 @@ var timeToEnjoyAnswerMs = 5000; //5secs // how long players have to read answer
 var currentQustion = null;
 
 //Socket.io emits this event when a connection is made.
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function(socket) {
 
-    socket.on('answer', function (data) {
+    socket.on('answer', function(data) {
         console.log('button ' + data.number);
 
         players.setAnswer(socket.id, parseInt(data.number));
     });
 
-    socket.on('playerJoin', function (data) {
+    socket.on('playerJoin', function(data) {
         var ip = socket.handshake.address.address;
         var p = players.addPlayer({
             playerId: socket.id,
             clientIp: ip,
             name: data.playerName
         });
-        console.log('SOCKET.IO player added: '+ p.name + ' from '+ ip + ' for socket '+ socket.id);
+        console.log('SOCKET.IO player added: ' + p.name + ' from ' + ip + ' for socket ' + socket.id);
         emitPlayerUpdate();
-        
+
         // TODO: Remove  - debug
 
-            
+
     });
-      
-    
-    
+
+
+
     socket.on('disconnect', function() {
         var pname = players.getPlayerName(socket.id);
-        console.log('SOCKET.IO player disconnect: '+ pname + ' for socket '+ socket.id);
+        console.log('SOCKET.IO player disconnect: ' + pname + ' for socket ' + socket.id);
         if (!pname) {
             // already disconnected
             return;
@@ -74,49 +74,37 @@ io.sockets.on('connection', function (socket) {
         emitPlayerUpdate();
     });
 
-     socket.on('startgame', function() { 
-             if(players.getPlayerCount() >= 1)
-            {   
-                emitNewQuestion();
-            }
-        });
-/*
-    socket.on('answer', function (data) { 
-        console.log('SOCKET.IO player answered: "'+ data.answer + '" for question: '+ data.question);
-        players.lastActive(socket.id);
-        // TODO: handle case where player might have already answered (damn hackers)
-        if (tq.isCorrect(data) && !players.winningSocket) {
-            console.log('SOCKET.IO player correct ! =========> : "'+ data.answer + '", '+ players[socket.id] + ' for socket '+ socket.id);
-            players.winningSocket = socket;
+    socket.on('startgame', function() {
+        if (players.getPlayerCount() >= 1) {
+            getQuestionMetaData();
         }
     });
-*/
+    /*
+        socket.on('answer', function (data) { 
+            console.log('SOCKET.IO player answered: "'+ data.answer + '" for question: '+ data.question);
+            players.lastActive(socket.id);
+            // TODO: handle case where player might have already answered (damn hackers)
+            if (tq.isCorrect(data) && !players.winningSocket) {
+                console.log('SOCKET.IO player correct ! =========> : "'+ data.answer + '", '+ players[socket.id] + ' for socket '+ socket.id);
+                players.winningSocket = socket;
+            }
+        });
+    */
 });
 
 
- 
 
-function emitNewQuestion() {
+
+function emitNewQuestion(q) {
     console.log(new Date().getTime());
     //players.winningSocket = null;
-    var index = Math.floor(Math.random()*3);
-    currentQustion = mathQuestions[index];
+    var index = Math.floor(Math.random() * 3);
 
-    io.sockets.emit('question', {
-        //totalTime: nextQuestionDelayMs,
-        //endTime: new Date().getTime() + nextQuestionDelayMs,
-         
-        choices: currentQustion.choices,
-        question: currentQustion.question
+    currentQustion = q;
+    io.sockets.emit("UnPresentAnswer");
+    io.sockets.emit('question', currentQustion);
 
-       // choices: ['aaa', 'bbbb', 'cccc', 'ddd'],
-       // question:'https://avatars1.githubusercontent.com/u/23655873?s=64&v=4'
-    });
-
-
-   // console.log(mathQuestions);
-
-    setTimeout(function(){
+    setTimeout(function() {
 
         players.updatePoints(currentQustion.answer);
         players.clearAnswers();
@@ -126,23 +114,23 @@ function emitNewQuestion() {
         io.sockets.emit("PresentAnswer", currentQustion.answer);
 
 
-    
+
         // var q = tq.getQuestionObj(true);
         // q.endTime = new Date().getTime() + timeToAnswerMs;
         // q.totalTime = timeToAnswerMs;
-        
+
         // io.sockets.emit('question', q);
-        
+
         // setTimeout(function(){
         //     emitAnswer();
         // }, timeToAnswerMs);
 
-    }, 5000); 
+    }, 5000);
 
-         setTimeout(function(){
-            io.sockets.emit("UnPresentAnswer");
-            emitNewQuestion();
-         },10000);
+    setTimeout(function() {
+        //emitNewQuestion();
+        getQuestionMetaData();
+    }, 10000);
 }
 
 /*
@@ -180,4 +168,149 @@ function emitAnswer() {
 function emitPlayerUpdate() {
     var playerData = players.getPlayerData();
     io.sockets.emit('players', playerData);
+}
+
+const NUM_OF_ANSWERS = 4;
+const SIZE_OF_SAMPLE = 100;
+
+function sampleDoc(data) {
+    var docset = data.SEGMENTS.JAGROOT.RESULT.DOCSET;
+
+    var randomIndex = Math.floor(Math.random() * SIZE_OF_SAMPLE);
+
+    return docset.DOC[randomIndex];
+}
+
+function getAnswers(trueAnswer, size) {
+
+    var isrSing = "http://primo.nli.org.il/PrimoWebServices/xservice/search/brief?institution=NNL&loc=local,scope:(NNL)&query=lsr08,exact,%D7%94%D7%A1%D7%A4%D7%A8%D7%99%D7%99%D7%94+%D7%94%D7%9C%D7%90%D7%95%D7%9E%D7%99%D7%AA+%D7%90%D7%A8%D7%9B%D7%99%D7%95%D7%9F+%D7%93%D7%9F+%D7%94%D7%93%D7%A0%D7%99&indx=1&bulkSize=" + SIZE_OF_SAMPLE + "&json=true";
+    var desc = null;
+    var answersList = [trueAnswer];
+
+    $.get(isrSing, function(data, status) {
+            for (var i = 1; i < size; i++) {
+                desc = null;
+                while (desc == null) {
+                    desc = sampleDoc(data).PrimoNMBib.record.display.title;
+                    if (answersList.indexOf(desc) > -1) {
+                        desc = null;
+                    }
+                }
+                answersList.push(desc);
+            }
+
+            return answersList;
+
+        },
+        async = false);
+
+    // [[trueAnswer, 0], [title2, 1], [title3, 2], [title4, 3]];
+}
+
+function shuffle(array) {
+
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function findNewIndex(array, originalIndex) {
+    var outIndex = -1;
+    var loopIndex = 0;
+    while (outIndex < 0) {
+        if (array[loopIndex][1] == originalIndex) {
+            outIndex = loopIndex;
+        }
+        loopIndex++;
+    }
+    return outIndex;
+}
+
+var request = require('request');
+
+url = "http://primo.nli.org.il/PrimoWebServices/xservice/search/brief?institution=NNL&loc=local,scope:(NNL)&query=lsr08,exact,%D7%94%D7%A1%D7%A4%D7%A8%D7%99%D7%99%D7%94+%D7%94%D7%9C%D7%90%D7%95%D7%9E%D7%99%D7%AA+%D7%90%D7%A8%D7%9B%D7%99%D7%95%D7%9F+%D7%93%D7%9F+%D7%94%D7%93%D7%A0%D7%99&indx=1&bulkSize=" + SIZE_OF_SAMPLE + "&json=true";
+
+request(url, function(error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+function getQuestionMetaData() {
+
+    console.log('ShowPictureClick:', this);
+
+    request(url, function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+
+
+
+        var recordId = JSON.parse(body).SEGMENTS.JAGROOT.RESULT.DOCSET.DOC[0].PrimoNMBib.record.control.recordid;
+
+        var trueDesc = JSON.parse(body).SEGMENTS.JAGROOT.RESULT.DOCSET.DOC[0].PrimoNMBib.record.display.title;
+
+        var imageManifest = "http://iiif.nli.org.il/IIIFv21/DOCID/" + recordId + "/manifest"
+
+        request(imageManifest, function(error, response, body) {
+
+            var imageUrl = JSON.parse(body).sequences[0].canvases[0].images[0].resource["@id"];
+            var url1 = "http://primo.nli.org.il/PrimoWebServices/xservice/search/brief?institution=NNL&loc=local,scope:(NNL)&query=lsr08,exact,%D7%94%D7%A1%D7%A4%D7%A8%D7%99%D7%99%D7%94+%D7%94%D7%9C%D7%90%D7%95%D7%9E%D7%99%D7%AA+%D7%90%D7%A8%D7%9B%D7%99%D7%95%D7%9F+%D7%93%D7%9F+%D7%94%D7%93%D7%A0%D7%99&indx=1&bulkSize=" + SIZE_OF_SAMPLE + "&json=true";
+            var desc = null;
+            var answersArray = [trueDesc];
+
+            request(url1, function(error, response, body) {
+                for (var i = 1; i < NUM_OF_ANSWERS; i++) {
+                    desc = null;
+                    while (desc == null) {
+                        desc = sampleDoc(JSON.parse(body)).PrimoNMBib.record.display.title;
+                        if (answersArray.indexOf(desc) > -1) {
+                            desc = null;
+                        }
+                    }
+                    answersArray.push(desc);
+                }
+                //answersArray = getAnswers(trueDesc, NUM_OF_ANSWERS);
+                var arrayWithIndex = [];
+                var arrayLength = answersArray.length;
+
+                // Add indexs along side the answers
+                for (var i = 0; i < arrayLength; i++)
+                    arrayWithIndex.push([answersArray[i], i]);
+
+                // Shuffle the answers
+                arrayWithIndex = shuffle(arrayWithIndex);
+                correctAnswerIndex = findNewIndex(arrayWithIndex, 0);
+
+                // Puts the 
+                for (var i = 0; i < arrayLength; i++)
+                    answersArray[i] = arrayWithIndex[i][0];
+
+                var obj = {
+                    question: imageUrl,
+                    choices: answersArray,
+                    answer: correctAnswerIndex
+                };
+
+                emitNewQuestion(obj);
+
+                return obj;
+            });
+        });
+    });
 }
